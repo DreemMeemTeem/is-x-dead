932389b38632491fd72109ee6b8c45a051ebeae4
Jesse Friedman <jesse@jesse.ws>
2017-05-21 03:39:15 -0400

    Adding full hash URL integration

--- js/app.js
+++ js/app.js
@@ -2,8 +2,14 @@ $(document).foundation();
 
 var searchDropdown = $('#search-dropdown');
 var textBox = $('#middle-label');
+var statusPage = $('#status-page');
 
 var personCache = {};
+var personNames = {};
+
+$(document).ready(function() {
+  window.onhashchange();
+})
 
 //textBox.get(0).onclick = undefined;
 textBox.on('input', function() {
@@ -17,79 +23,123 @@ textBox.focus(function() {
   console.log('focus')
   searchDropdown.addClass('is-open');
 });
-textBox.blur(function() {
+/*textBox.focusout(function() {
   console.log('blur')
   searchDropdown.removeClass('is-open');
-});
+});*/
 
 window.onhashchange = function() {
-  console.log(location.hash)
+  if (location.hash.length > 1) {
+    textBox.attr('placeholder', '');
+    var id = unPrettifyId(location.hash.slice(1));
+    $.getJSON('https://uprvn9yff5.execute-api.us-east-1.amazonaws.com/v1/query?ids=' + id, function(people) {
+      if (people.hasOwnProperty(id)) {
+        var person = people[id];
+        
+
+        var label = person.name || personNames[id] || '';
+        textBox.val(label);
+        $('#person-name').text(label);
+        
+        $('#status-display').removeClass('alive dead unknown').addClass(person.status);
+        if (person.image) {
+          $('.profilepic').css('background-image', 'url(' + person.image + ')').show();
+        } else {
+          $('.profilepic').hide();
+        }
+
+
+        if (person.birthYear) {
+          $('#yob').text(person.birthYear);
+        } else {
+          $('#yob').text('Unknown');
+        }
+        if (person.deathYear) {
+          $('#yod').text(person.deathYear);
+        } else {
+          $('#yod').text('Unknown');
+        }
+        if (person.age) {
+          $('#age').text(person.age);
+        } else {
+          $('#age').text('Unknown');
+        }
+
+        if (person.status === 'alive') {
+          $('#yod').parent().hide();
+          $('#age').prev().text('Age');
+        } else if (person.status === 'dead') {
+          $('#yod').parent().show();
+          $('#age').prev().text('Age at death');
+        } else if (person.status === 'unknoen') {
+          $('#yod').parent().show();
+          $('#age').prev().text('Age');
+        }
+
+        console.log(person)
+      } else {
+        textBox.val('placeholder', personNames[id] || '');
+        $('#person-name').text(personNames[id] || '');
+        
+        $('#yob').text('Unknown');
+        $('#yod').text('Unknown');
+        $('#age').text('Unknown');
+      }
+      statusPage.show();
+    });
+  } else {
+    statusPage.hide();
+  }
 };
+
+function displayDetails(id) {
+
+}
 
 function autocompleteText() {
   $.getJSON('https://uprvn9yff5.execute-api.us-east-1.amazonaws.com/v1/autocomplete?query=' + textBox.val(), function(data) {
     searchDropdown.empty();
     data.forEach(function(person) {
       var prettyId = prettifyId(person.id);
-      var aElem = $('<a>').attr('person-id', prettyId).prop('href', '#' + prettyId);
+      var spanElem = $('<span>').attr('person-id', prettyId);
       var liElem = $('<li>');
       if (person.image) {
         liElem.append($('<img>').prop('src', person.image));
       }
+
+      personNames[person.id] = person.name;
+
       liElem.append($('<hgroup>').append($('<b>').text(person.name)).append($('<p>').text(person.description)));
-      liElem.append($('<div>').addClass('status').append($('<div>').addClass('spinner')).append($('<div>').addClass('result')));
-      aElem.click(function() {
-        console.log(window.location.hash)
-        window.location.hash = $(this).attr('person-id');
-      })
-      aElem.append(liElem);
-      searchDropdown.append(aElem);
+      liElem.append($('<div>').addClass('status').append($('<div>').addClass('result unknown')));
+      spanElem.append(liElem);
+      searchDropdown.append(spanElem);
     });
     searchDropdown.addClass('is-open');
     loadStatuses();
+    searchDropdown.children('span').on('click', function() {
+      window.location.hash = $(this).attr('person-id');
+      searchDropdown.removeClass('is-open');
+    });
   });
-
-
-
-  /*
-            <a href="http://google.com">
-              <li>
-                <img src="https://media3.s-nbcnews.com/j/newscms/2016_20/1541946/160518-trump-portrait-jsw-145p_1c226e6636be4572928409c157f0d50a.nbcnews-ux-2880-1000.jpg" />
-                <div>
-                  <b>Donald Trump</b>
-                  <p>Donald John Trump (born June 14, 1946) is the 45th and current President of the United States. Before entering politics, he was a businessman and television personality. </p>
-                </div>
-                <div class="status">
-                  <!-- Add "doneload" class to spinner when finished to hide it, or just set display to none-->
-                  <div class="spinner doneload">
-                    <div class="double-bounce"></div>
-                  </div>
-                  <!--Add/change to "isalive" or "isdead" after removing the spinner.-->
-                  <div class="result isalive"></div>
-              </div>
-              </li>
-            </a>
-  */
 }
 
 function loadStatuses() {
   var ids = searchDropdown.children().map(function() {
     return unPrettifyId($(this).attr('person-id'))
   }).toArray();
-  $.getJSON('https://uprvn9yff5.execute-api.us-east-1.amazonaws.com/v1/query?ids=' + ids.join(','), function(people) {
-    console.log(people)
-    for (id in people) {
-      console.log(id)
-      var person = people[id];
-      personCache[id] = person;
-      var elem = searchDropdown.find('[person-id="' + prettifyId(id) + '"]');
-      console.log(elem)
-      if (elem) {
-        elem.find('.spinner').hide();
-        elem.find('.result').addClass(person.status).show();
+  if (ids.length > 0) {
+    $.getJSON('https://uprvn9yff5.execute-api.us-east-1.amazonaws.com/v1/query?ids=' + ids.join(','), function(people) {
+      console.log(people)
+      for (id in people) {
+        var person = people[id];
+        personCache[id] = person;
+        var elem = searchDropdown.find('[person-id="' + prettifyId(id) + '"]');
+        if (elem) {
+          elem.find('.result').removeClass('unknown').addClass(person.status).show();
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 function prettifyId(id) {
