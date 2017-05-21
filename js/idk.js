1ec76f467a5b84cc383b8cadfb35bef48263d6ef
IncognitoCactus <abc811@gmail.com>
2017-05-21 04:44:30 -0400

    Added option to remove dabs and confetti, added tagline js

---
+++ js/idk.js
@@ -0,0 +1,7 @@
+var aliveName = ["Great!", "Amazing!", "Woohoo!", "Lovely!", "Congrats!"]; {
+  var rand = aliveName[Math.floor(Math.random() * aliveName.length)];
+}
+
+var deadName = ["I'm so sorry.", "My condolences.", "Bummer.", "Apologies.", "...No sign of life here." ]; {
+  var rand = deadName[Math.floor(Math.random() * deadName.length)];
+}
